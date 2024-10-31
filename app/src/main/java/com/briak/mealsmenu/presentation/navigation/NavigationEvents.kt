c801172f1133dedd25b7c32febfd012e8662290d
Kristina Efros <kristina.efros@urban-connect.ch>
2024-10-31 09:59:19 +0100

    Add bottom navigation

--- app/src/main/java/com/briak/mealsmenu/presentation/navigation/NavigationEvents.kt
+++ app/src/main/java/com/briak/mealsmenu/presentation/navigation/NavigationEvents.kt
@@ -1,13 +1,50 @@
 package com.briak.mealsmenu.presentation.navigation
 
+import com.briak.mealsmenu.R
 import kotlinx.serialization.Serializable
 
-@Serializable object Overview
+// Bottom navigation root screens
 
-@Serializable object MealDetails
+@Serializable
+object OverviewScreens
+
+@Serializable
+object FavouritesScreens
+
+@Serializable
+sealed class BottomScreens<T>(
+    val name: String,
+    val icon: Int,
+    val route: T,
+) {
+    @Serializable
+    data object Overview : BottomScreens<OverviewScreens>(
+        name = "Overview",
+        icon = R.drawable.ic_menu_book,
+        route = OverviewScreens,
+    )
+
+    @Serializable
+    data object Favourites : BottomScreens<FavouritesScreens>(
+        name = "Favourites",
+        icon = R.drawable.ic_favorite,
+        route = FavouritesScreens,
+    )
+}
+
+// Overview screens
+
+@Serializable
+object MealDetails
 
 sealed class OverviewNavigationEvent {
     data object OnBackPressed : OverviewNavigationEvent()
 
     data object OnMealClicked : OverviewNavigationEvent()
+}
+
+// Favourites screens
+
+sealed class FavouritesNavigationEvent {
+    data object OnBackPressed : FavouritesNavigationEvent()
 }
--- app/src/main/java/com/briak/mealsmenu/presentation/screens/main/MainActivity.kt
+++ app/src/main/java/com/briak/mealsmenu/presentation/screens/main/MainActivity.kt
@@ -7,18 +7,34 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionLayout
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
+import com.briak.mealsmenu.presentation.navigation.BottomScreens
+import com.briak.mealsmenu.presentation.navigation.FavouritesScreens
 import com.briak.mealsmenu.presentation.navigation.MealDetails
 import com.briak.mealsmenu.presentation.navigation.NavigationAnim
-import com.briak.mealsmenu.presentation.navigation.Overview
 import com.briak.mealsmenu.presentation.navigation.OverviewNavigationEvent
+import com.briak.mealsmenu.presentation.navigation.OverviewScreens
+import com.briak.mealsmenu.presentation.screens.favourites.FavouritesScreen
 import com.briak.mealsmenu.presentation.screens.mealdetails.MealDetailsScreen
 import com.briak.mealsmenu.presentation.screens.overview.OverviewScreen
 import com.briak.mealsmenu.presentation.theme.AppTheme
@@ -46,36 +62,83 @@ class MainActivity : ComponentActivity() {
     private fun Navigation() {
         val baseNavController = rememberNavController()
 
-        SharedTransitionLayout {
-            NavHost(
-                navController = baseNavController,
-                startDestination = Overview,
-                enterTransition = { NavigationAnim.forthEnter(targetState.destination.route) },
-                exitTransition = { NavigationAnim.forthExit(targetState.destination.route) },
-                popEnterTransition = { NavigationAnim.backEnter(initialState.destination.route) },
-                popExitTransition = { NavigationAnim.backExit(initialState.destination.route) },
-            ) {
-                composable<Overview> {
-                    OverviewScreen(
-                        sharedTransitionScope = this@SharedTransitionLayout,
-                        animatedVisibilityScope = this@composable,
-                    ) {
-                        handleOverviewNavigation(it, baseNavController)
+        val bottomScreens =
+            remember {
+                listOf(
+                    BottomScreens.Overview,
+                    BottomScreens.Favourites,
+                )
+            }
+
+        Scaffold(
+            bottomBar = {
+                NavigationBar {
+                    val navBackStackEntry by baseNavController.currentBackStackEntryAsState()
+                    val currentDestination = navBackStackEntry?.destination
+                    bottomScreens.forEach { item ->
+                        NavigationBarItem(
+                            icon = {
+                                Icon(
+                                    imageVector = ImageVector.vectorResource(item.icon),
+                                    contentDescription = item.name,
+                                )
+                            },
+                            label = { Text(item.name) },
+                            selected =
+                                currentDestination?.hierarchy?.any { it.route == item.route::class.qualifiedName }
+                                    ?: false,
+                            onClick = {
+                                baseNavController.navigate(item.route) {
+                                    popUpTo(baseNavController.graph.findStartDestination().id) {
+                                        saveState = true
+                                    }
+                                    launchSingleTop = true
+                                    restoreState = true
+                                }
+                            },
+                        )
                     }
                 }
-                composable<MealDetails> {
-                    MealDetailsScreen(
-                        sharedTransitionScope = this@SharedTransitionLayout,
-                        animatedVisibilityScope = this@composable,
-                    ) {
-                        handleOverviewNavigation(it, baseNavController)
+            },
+        ) { innerPadding ->
+            SharedTransitionLayout {
+                NavHost(
+                    navController = baseNavController,
+                    startDestination = OverviewScreens,
+                    enterTransition = { NavigationAnim.forthEnter(targetState.destination.route) },
+                    exitTransition = { NavigationAnim.forthExit(targetState.destination.route) },
+                    popEnterTransition = { NavigationAnim.backEnter(initialState.destination.route) },
+                    popExitTransition = { NavigationAnim.backExit(initialState.destination.route) },
+                ) {
+                    navigation<OverviewScreens>(startDestination = BottomScreens.Overview) {
+                        composable<BottomScreens.Overview> {
+                            OverviewScreen(
+                                sharedTransitionScope = this@SharedTransitionLayout,
+                                animatedVisibilityScope = this@composable,
+                            ) {
+                                handleOverviewNavigation(it, baseNavController)
+                            }
+                        }
+                        composable<MealDetails> {
+                            MealDetailsScreen(
+                                sharedTransitionScope = this@SharedTransitionLayout,
+                                animatedVisibilityScope = this@composable,
+                            ) {
+                                handleOverviewNavigation(it, baseNavController)
+                            }
+                        }
+                    }
+                    navigation<FavouritesScreens>(startDestination = BottomScreens.Favourites) {
+                        composable<BottomScreens.Favourites> {
+                            FavouritesScreen()
+                        }
                     }
                 }
             }
         }
     }
 
-    fun handleOverviewNavigation(
+    private fun handleOverviewNavigation(
         event: OverviewNavigationEvent,
         navController: NavController,
     ) {
